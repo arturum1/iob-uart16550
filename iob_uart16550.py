1c3f42126625a905d64b832828f598f01582ffd3
Artur Nóbrega <arturnobrega1999@gmail.com>
2026-02-25 23:51:09 +0000

    chore(version): Upgrade version to 0.1.5 Will allow us to publish a new fusesoc version with license attribute

--- iob_uart16550.py
+++ iob_uart16550.py
@@ -17,7 +17,7 @@ def setup(py_params_dict):
         "name": NAME,
         "generate_hw": True,
         "description": "IObundle's adaptation of the UART16550 from: https://opencores.org/projects/uart16550. The original sources for this core and public key signature are available at: https://github.com/IObundle/iob-uart16550.",
-        "version": "0.1.4",
+        "version": "0.1.5",
         "board_list": ["iob_aes_ku040_db_g"],
         #
         # Confs
